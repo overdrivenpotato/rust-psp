11ed87d690d1b9c6997f1f5ecce5c4aa37e4c9a5
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-05-14 20:35:08 -0400

    Improve bindgen script to camelCase arguments

--- bindgen.sh
+++ bindgen.sh
@@ -9,13 +9,24 @@ header() {
         -I /usr/local/pspdev/psp/include \
         -include sys/types.h -include pspkerneltypes.h \
         | sed -re '
+            # Format doc comments as triple slashes
             s/#\[doc = "([^"]*)"\]/\/\/\/\1/g
+
+            # Remove redundant raw types
             s/::std::os::raw::c_int/i32/g
             s/::std::os::raw::c_uint/u32/g
             s/::std::os::raw::c_void/c_void/g
+
+            s/uint/u32/g
+            s/SceSize/usize/g
+            s/SceSSize/isize/g
             s/pub fn/pub unsafe fn/
             s/@param (\w*)/`\1`/
             s/@return (.*)/# Return Value\n    \/\/\/\n    \/\/\/ \1/
+
+            # Delete redundant types resulting from previous replacements
+            /pub type (\w+) = (\1);/d
+            /pub type (usize|isize) =/d
         ' \
         | awk '
             # Bindgen generates multiple extern blocks so we want to merge them.
@@ -69,7 +80,7 @@ cat <(pspModule $2) <(header $1 | rustfmt) \
             }
         }
 
-        function camelToSnake(s,    last) {
+        function camelToSnake(s,    last, i) {
             out = ""
 
             for (i = 0; i < length(s); i++) {
@@ -113,14 +124,55 @@ cat <(pspModule $2) <(header $1 | rustfmt) \
 
         /pub unsafe fn/ {
             # Function name start and end index
-            start = match($0, "fn ") + 3
-            end = match(substr($0, start), "\\(") + start
-            camel = substr($0, start, end - start - 1)
+            fn_start = match($0, "fn ") + 3
+            fn_end = match(substr($0, fn_start), "\\(") + fn_start
+            fn_camel = substr($0, fn_start, fn_end - fn_start - 1)
 
-            printf "    #[psp(" nid_map[camel] ")]\n"
-            printf substr($0, 1, start - 1)
-            printf camelToSnake(camel)
-            print substr($0, end - 1)
+            printf "    #[psp(" nid_map[fn_camel] ")]\n"
+            printf substr($0, 1, fn_start - 1)
+            printf camelToSnake(fn_camel) "("
+
+            # Substring of the argument list
+            args = substr($0, fn_end, match(substr($0, fn_end), ")") - 1)
+
+            args_multiline = match(args, "\n")
+            fn_return = fn_end + length(args)
+
+            if (args_multiline) printf "\n"
+
+            # Fix the argument names
+            while (1) {
+                # These are indices
+                arg_start = match(args, "[^[:space:]]")
+                arg_colon = match(args, ":")
+                arg_comma = match(args, ",")
+
+                # Exit if no more arguments
+                if (!arg_start) break
+
+                arg_len = arg_comma - arg_start
+
+                # Get the camelCase and snake_case variants of the argument name
+                arg_camel = substr(args, arg_start, arg_colon - arg_start)
+                arg_snake = camelToSnake(arg_camel)
+
+                # Recreate the argument definition with the correct case
+                if (arg_comma) arg = substr(args, arg_start, arg_len)
+                else           arg = substr(args, arg_start)
+                sub(arg_camel, arg_snake, arg)
+
+                if (args_multiline) printf "        " arg ",\n"
+                else if (arg_comma) printf arg ","
+                else                printf arg
+
+                # Advance the argument substring onto the next one
+                if (arg_comma) args = substr(args, arg_comma + 1)
+                else           break
+            }
+
+            if (args_multiline) printf "    "
+
+            print substr($0, fn_return)
             printf "\n"
 
             next
