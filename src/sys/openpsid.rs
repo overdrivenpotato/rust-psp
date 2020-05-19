189fce1b00b4abddc78bbbe3cd8fc321d35a656f
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-05-19 02:40:39 -0400

    Add OpenPSID (#18)
    Add OpenPSID

---
+++ src/sys/openpsid.rs
@@ -0,0 +1,14 @@
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct OpenPSID {
+    pub data: [u8; 16usize],
+}
+
+sys_lib! {
+    #![name = "sceOpenPSID"]
+    #![flags = 0x4001]
+    #![version = (0x00, 0x11)]
+
+    #[psp(0xC69BEBCE)]
+    pub unsafe fn sce_open_psid_get_open_psid(openpsid: *mut OpenPSID) -> i32;
+}
