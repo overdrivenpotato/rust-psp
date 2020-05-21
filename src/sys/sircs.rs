2c2c82b2d57da9c087d929cd6827602177468eb8
Paul Sajna <sajattack@gmail.com>
2020-05-21 00:43:13 -0700

    Add sircs module

---
+++ src/sys/sircs.rs
@@ -0,0 +1,20 @@
+/// Sony Integrated Remote Control System Library 
+/// This module contains the imports for the kernel's remote control routines.
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct SircsData {
+    pub type_: u8,
+    pub cmd: u8,
+    pub dev: u16,
+}
+
+sys_lib! {
+    #![name = "sceSircs"]
+    #![flags = 0x4001]
+    #![version = (0x00, 0x00)]
+
+    #[psp(0x71EEF62D)]
+    pub unsafe fn sce_sircs_send(sd: *mut sircs_data, count: i32) -> i32;
+}
+
