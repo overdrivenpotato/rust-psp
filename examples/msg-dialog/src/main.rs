52ab79f5a5c6b6af0f9144eed54341e57de47461
Paul Sajna <sajattack@gmail.com>
2020-05-31 00:50:06 -0700

    fix msgdialog sample

--- examples/msg-dialog/src/main.rs
+++ examples/msg-dialog/src/main.rs
@@ -14,28 +14,7 @@ use core::ffi::c_void;
 
 psp::module!("sample_module", 1, 1);
 
-
-unsafe extern "C" fn exit_callback(_arg1: i32, _arg2: i32, _common: *mut c_void) -> i32 {
-    kernel::sce_kernel_exit_game();
-    0
-}
-
-unsafe extern "C" fn update_callback(_argc: usize, _argv: *mut c_void) -> i32 {
-    let cbid: SceUid;
-    let callback_name: &[u8; 14] = b"Exit Callback\0";
-    cbid = kernel::sce_kernel_create_callback(
-        callback_name as *const u8,
-        exit_callback,
-        core::ptr::null_mut() as *mut c_void
-    );
-    kernel::sce_kernel_register_exit_callback(cbid);
-    kernel::sce_kernel_sleep_thread_cb();
-    0
-}
-
 fn psp_main() {
-    psp::dprintln!("test");
-
     let thid: SceUid;
     let callback_name: &[u8; 14] = b"update_thread\0";
     unsafe {
@@ -54,8 +33,22 @@ fn psp_main() {
     }
 
     let base_size = core::mem::size_of::<DialogCommon>();
+    static mut list: psp::Align16<[u32; 262144]> = psp::Align16([0;262144]);
+
+    unsafe {
+        gu::sce_gu_init(); 
+        gu::sce_gu_start(gu::Context::Direct, &mut list as *mut _ as *mut c_void);
+        gu::sce_gu_draw_buffer(gu::PixelFormat::Psm8888, core::ptr::null_mut(), 512);
+        gu::sce_gu_finish();
+        gu::sce_gu_sync(gu::SyncMode::SyncFinish, gu::SyncBehaviorWhat::SyncWhatDone);
+        display::sce_display_wait_vblank_start();
+        gu::sce_gu_display(true);
+    }
+
+    let dialog_size = core::mem::size_of::<MsgDialogParams>();
+>>>>>>> 95e55b6... fix msgdialog sample
     let base = DialogCommon {
-        size: base_size as u32,
+        size: dialog_size as u32,
         language: 1, // english, TODO add this as an enum later
         button_swap: 1, // X to accept 
         graphics_thread: 0x11, // magic number stolen from pspsdk example
@@ -87,14 +80,14 @@ fn psp_main() {
 
     loop {
         let status = unsafe {utility::sce_utility_msg_dialog_get_status()};
-        psp::dprintln!("{}", status);
         // TODO figure out these values
         match status {
             2 => unsafe{utility::sce_utility_msg_dialog_update(1)},
             3 => unsafe{utility::sce_utility_msg_dialog_shutdown_start()},
-            0 => break,
+            0 => {break},
             _ => {},
         }
         unsafe {display::sce_display_wait_vblank_start();}
     }
+    unsafe { kernel::sce_kernel_exit_game(); }
 }
