dbf643cfd64923755cf20bcde4c648e420005653
Paul Sajna <sajattack@gmail.com>
2020-05-31 23:52:01 -0700

    remove usage from rainbow

--- examples/rainbow/src/main.rs
+++ examples/rainbow/src/main.rs
@@ -18,8 +18,8 @@ fn psp_main() {
         display::sce_display_set_mode(display::DisplayMode::Lcd, DISPLAY_WIDTH, DISPLAY_HEIGHT);
         VRAM = (0x4000_0000u32 | ge::sce_ge_edram_get_addr() as u32) as *mut u32;
         display::sce_display_set_frame_buf(VRAM as *const u8, BUFFER_WIDTH, display::DisplayPixelFormat::_8888, display::DisplaySetBufSync::NextFrame);
-        let time = psp::benchmark(|| {
-            //display::sce_display_wait_vblank_start();
+        loop {
+            display::sce_display_wait_vblank_start();
             for pos in 0..255  {
                 let color = wheel(pos);
 
@@ -27,8 +27,7 @@ fn psp_main() {
                     *VRAM.add(i) = color;
                 }
             }
-        }, 10);
-        psp::dprintln!("{:?}", time);
+        }
     }
 }
 
