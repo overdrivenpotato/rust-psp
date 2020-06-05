83164abd483b41a10aabce8556abca390c282e92
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-06-05 17:18:13 -0400

    Add pack-pbp and prxgen

---
+++ cargo-psp/src/bin/pack-pbp.rs
@@ -0,0 +1,146 @@
+use clap::{App, Arg, AppSettings};
+use std::{fs, mem};
+
+const SIGNATURE: [u8; 4] = *b"\0PBP";
+const VERSION: u32 = 0x1_0000;
+
+struct PbpHeader {
+    signature: [u8; 4],
+    version: u32,
+    offsets: [u32; 8],
+}
+
+impl PbpHeader {
+    fn to_bytes(self) -> [u8; mem::size_of::<Self>()] {
+        let mut bytes = [0; mem::size_of::<Self>()];
+
+        bytes[0..4].copy_from_slice(&self.signature);
+        bytes[4..8].copy_from_slice(&self.version.to_le_bytes());
+
+        for (i, offset) in self.offsets.iter().enumerate() {
+            let idx = i * 4 + 8;
+            bytes[idx..idx + 4].copy_from_slice(&offset.to_le_bytes());
+        }
+
+        bytes
+    }
+}
+
+fn main() {
+    let matches = App::new("pack-pbp")
+        .version("0.1")
+        .author("Marko Mijalkovic <marko.mijalkovic97@gmail.com>")
+        .about("Create Sony PSP packages")
+        .setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("output.pbp")
+                .takes_value(true)
+                .help("Output PBP file")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("param.sfo")
+                .takes_value(true)
+                .help("Input PARAM.SFO file created with mksfo")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("icon0.png")
+                .takes_value(true)
+                .help("Optional XMB icon")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("icon1.pmf")
+                .takes_value(true)
+                .help("Optional animated XMB icon")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("pic0.png")
+                .takes_value(true)
+                .help("Optional XMB background (overlayed on top of PIC1.PNG)")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("pic1.png")
+                .takes_value(true)
+                .help("Optional XMB background")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("snd0.at3")
+                .takes_value(true)
+                .help("Optional XMB music (when present, sound from ICON1.PMF is ignored)")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("data.psp")
+                .takes_value(true)
+                .help("Executable file")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("data.psar")
+                .takes_value(true)
+                .help("Optional archive data")
+                .required(true)
+        )
+        .get_matches();
+
+    let read = |name: &str| {
+        let value = matches.value_of(name).unwrap();
+
+        if value == "NULL" {
+            return None;
+        }
+
+        match fs::read(value) {
+            Ok(b) => Some(b),
+            Err(e) => panic!("failed to read {}: {}", value, e),
+        }
+    };
+
+    let output_file = matches.value_of("output.pbp").unwrap();
+
+    let files = vec![
+        read("param.sfo"),
+        read("icon0.png"),
+        read("icon1.pmf"),
+        read("pic0.png"),
+        read("pic1.png"),
+        read("snd0.at3"),
+        read("data.psp"),
+        read("data.psar"),
+    ];
+
+    let mut payload = Vec::new();
+    let mut offsets = [0; 8];
+    let mut current_offset = mem::size_of::<PbpHeader>() as u32;
+
+    for (i, bytes) in files.into_iter().enumerate() {
+        offsets[i] = current_offset;
+
+        if let Some(bytes) = bytes {
+            let len = bytes.len();
+            payload.extend(bytes);
+            current_offset += len as u32;
+        }
+    }
+
+    let header = PbpHeader {
+        signature: SIGNATURE,
+        version: VERSION,
+        offsets,
+    };
+
+    let mut output = Vec::new();
+    output.extend(&header.to_bytes()[..]);
+    output.extend(payload);
+
+    if let Err(e) = fs::write(output_file, &output) {
+        panic!("couldn't write to {}: {}", output_file, e);
+    }
+
+    println!("pack-pbp: Wrote {} bytes", output.len());
+}
---
+++ cargo-psp/src/bin/prxgen.rs
@@ -0,0 +1,216 @@
+use std::{fs, path::Path, collections::HashMap};
+use goblin::elf32::{
+    header::Header, reloc::Rel,
+    section_header::{SectionHeader, SHT_REL, SHF_ALLOC},
+    program_header::{ProgramHeader, PT_LOAD},
+};
+use scroll::{Endian, ctx::{TryIntoCtx, TryFromCtx}};
+use clap::{App, Arg, AppSettings};
+
+const PRX_ELF_TYPE: u16 = 0xffa0;
+const PRX_SHT_REL: u32 = 0x700000A0;
+
+fn main() {
+    let matches = App::new("prxgen")
+        .version("0.1")
+        .author("Marko Mijalkovic <marko.mijalkovic97@gmail.com>")
+        .about("Convert PSP ELF files to PRX format")
+        .setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("in_file.elf")
+                .takes_value(true)
+                .help("Input ELF file")
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("out_file.prx")
+                .takes_value(true)
+                .help("Output PRX file")
+                .required(true)
+        )
+        .get_matches();
+
+    let mut prx_gen = PrxGen::load(matches.value_of("in_file.elf").unwrap());
+    prx_gen.modify();
+    prx_gen.save(matches.value_of("out_file.prx").unwrap());
+}
+
+struct PrxGen {
+    elf_bytes: Vec<u8>,
+
+    header: Header,
+    section_headers: Vec<SectionHeader>,
+    program_headers: Vec<ProgramHeader>,
+
+    // Section index -> Vec<Rel>
+    relocations: HashMap<usize, Vec<Rel>>,
+
+    module_info_offset: u32,
+}
+
+impl PrxGen {
+    /// Load the input ELF file and parse important structures.
+    fn load<P: AsRef<Path>>(path: P) -> Self {
+        let bytes = fs::read(path).unwrap();
+        let header = Header::parse(&bytes).unwrap();
+        let section_headers = SectionHeader::from_bytes(
+            &bytes[header.e_shoff as usize..],
+            header.e_shnum as usize,
+        );
+        let program_headers = ProgramHeader::from_bytes(
+            &bytes[header.e_phoff as usize..],
+            header.e_phnum as usize,
+        );
+
+        let module_info_offset = {
+            let section_names = {
+                // Section header string table
+                let sh_string_table = section_headers[header.e_shstrndx as usize];
+
+                let start_idx = sh_string_table.sh_offset as usize;
+                let end_idx = start_idx + sh_string_table.sh_size as usize;
+
+                let strings = &bytes[start_idx..end_idx];
+
+                strings
+                    .split(|b| *b == 0)
+                    .map(Vec::from)
+                    .map(String::from_utf8)
+                    // All section header names should be utf8 or something is
+                    // severely wrong.
+                    .map(Result::unwrap)
+                    .collect::<Vec<_>>()
+            };
+
+            section_headers
+                .iter()
+                .enumerate()
+                .find_map(|(i, sh)| {
+                    if section_names[i] == ".rodata.sceModuleInfo" {
+                        Some(sh.sh_offset)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap()
+        };
+
+        let relocations = section_headers.iter()
+            .enumerate()
+            .filter(|(_, sh)| sh.sh_type == SHT_REL)
+            .map(|(i, sh)| {
+                let start_idx = sh.sh_offset as usize;
+                let end_idx = sh.sh_size as usize + start_idx;
+
+                let relocs = (&bytes[start_idx..end_idx])
+                    .chunks(8)
+                    .map(|rel_bytes| {
+                        Rel::try_from_ctx(rel_bytes, Endian::Little).unwrap().0
+                    })
+                    .collect();
+
+                (i, relocs)
+            })
+            .collect();
+
+        Self {
+            elf_bytes: bytes,
+            header,
+            section_headers,
+            program_headers,
+            relocations,
+            module_info_offset,
+        }
+    }
+
+    /// Modify the inner structures to create a PRX format file.
+    fn modify(&mut self) {
+        // Change ELF type
+        self.header.e_type = PRX_ELF_TYPE;
+
+        // Immutable copy for indexing.
+        let section_headers = self.section_headers.clone();
+
+        // Change relocation section types
+        for section_header in &mut self.section_headers {
+            if section_header.sh_type == SHT_REL {
+                let sh_target = section_headers[section_header.sh_info as usize];
+
+                if sh_target.sh_flags & SHF_ALLOC != 0 {
+                    section_header.sh_type = PRX_SHT_REL;
+                }
+            }
+        }
+
+        // Change all relocation types.
+        for (i, rels) in &mut self.relocations {
+            if self.section_headers[*i].sh_type == PRX_SHT_REL {
+                for rel in rels {
+                    // Set upper 24 bits to 0 (OFS_BASE, ADDR_BASE).
+                    rel.r_info &= 0xff;
+                }
+            }
+        }
+
+        // Change first program header physical address to `.rodata.sceModuleInfo` file offset
+        // TODO: Kernel mode support
+        self.program_headers[0].p_paddr = self.module_info_offset;
+
+        // Merge all segments. The PSP seems to only be able to handle 1 `LOAD`
+        // segment. This code assumes that all load segments appear sequentially
+        // and that the first segment is loaded at virtual address 0. Assertions
+        // ensure this is the case.
+        {
+            // First segment needs to be loaded to 0.
+            assert_eq!(0, self.program_headers[0].p_vaddr);
+
+            let load_segments = || self.program_headers.iter()
+                .filter(|ph| ph.p_type == PT_LOAD);
+
+            // Sum the `memsz` values.
+            let mem_size = load_segments()
+                .map(|ph| ph.p_memsz)
+                .sum::<u32>();
+
+            let file_size = load_segments()
+                .map(|ph| ph.p_filesz)
+                .sum::<u32>();
+
+            self.program_headers[0].p_filesz = file_size;
+            self.program_headers[0].p_memsz = mem_size;
+
+            self.header.e_phnum = 1;
+        }
+    }
+
+    /// Write out the changes to a file.
+    fn save<P: AsRef<Path>>(self, output: P) {
+        let mut bytes = self.elf_bytes;
+
+        // Write header to buffer
+        self.header.try_into_ctx(&mut bytes, Endian::Little).unwrap();
+
+        // Write relocations to buffer
+        for (i, rels) in self.relocations {
+            let offset = self.section_headers[i].sh_offset as usize;
+
+            for (j, rel) in rels.into_iter().enumerate() {
+                rel.try_into_ctx(&mut bytes[offset + j * 8..], Endian::Little).unwrap();
+            }
+        }
+
+        // Write section headers to buffer
+        for (i, section_header) in self.section_headers.into_iter().enumerate() {
+            let offset = self.header.e_shoff as usize + i * self.header.e_shentsize as usize;
+            section_header.try_into_ctx(&mut bytes[offset..], Endian::Little).unwrap();
+        }
+
+        // Write program headers to buffer
+        for (i, program_header) in self.program_headers.into_iter().enumerate() {
+            let offset = self.header.e_phoff as usize + i * self.header.e_phentsize as usize;
+            program_header.try_into_ctx(&mut bytes[offset..], Endian::Little).unwrap();
+        }
+
+        fs::write(output, bytes).unwrap();
+    }
+}
