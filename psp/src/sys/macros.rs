0c938c7adefcaf31f71153ef3fe5e2b4975e56b3
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-06-15 01:48:08 -0400

    Mark sys import handlers extern

--- psp/src/sys/macros.rs
+++ psp/src/sys/macros.rs
@@ -183,7 +183,8 @@ macro_rules! psp_extern {
         $(
             $(#[$attr])*
             #[allow(non_snake_case)]
-            pub unsafe fn $name($($arg : $arg_ty),*) $(-> $ret)? {
+            #[no_mangle]
+            pub unsafe extern fn $name($($arg : $arg_ty),*) $(-> $ret)? {
                 #[cfg(target_os = "psp")]
                 {
                     psp_extern!(
