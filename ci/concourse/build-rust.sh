62a6ae594e8ce752457aaab21faa21094b0bd60c
Glenn Hope <glenn.alexander.hope@gmail.com>
2020-07-16 21:29:04 -0700

    Fix build script

--- ci/concourse/build-rust.sh
+++ ci/concourse/build-rust.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
-set -euo
+set -euxo pipefail
 
 if [ -d repo/ci/ ]; then
+    CI="1"
+else
+    CI="0"
+fi
+
+if true; then
+    RELEASE="1"
+else
+    RELEASE="0"
+fi
+
+if [ "$CI" = "1" ]; then
     export PREFIX="repo/"
     export HOMEDIR="$(pwd)"
+    export CARGO_HOME="${HOMEDIR}"/.cargo
+    export XARGO_HOME="${HOMEDIR}"/.xargo
 else
     export PREFIX="rust-psp/"
     export HOMEDIR="${HOME}"
 fi
 
-export CARGO_HOME="${HOMEDIR}"/.cargo
-export XARGO_HOME="${HOMEDIR}"/.xargo
 
 pushd ${PREFIX}cargo-psp/
-cargo build --release
+if [ "$RELEASE" = "1" ]; then
+    cargo build --release
+else
+    cargo build
+fi
 popd
+
+PATH="${HOMEDIR}/${PREFIX}/target/release:${PATH}"
 
 pushd ${PREFIX}ci/tests
-${HOMEDIR}/${PREFIX}target/release/cargo-psp
+[ -f Xargo.toml ] && rm Xargo.toml
+if [ "$RELEASE" = "1" ]; then
+    # TODO: add release flag? did not work with it added.
+    ${HOMEDIR}/${PREFIX}target/release/cargo-psp
+else
+    ${HOMEDIR}/${PREFIX}target/debug/cargo-psp
+fi
 popd
 
-cp -r ${PREFIX}ci/tests/target/mipsel-sony-psp/release/* release/
+if [ "$CI" = "1" ]; then
+    cp -r ${PREFIX}ci/tests/target/mipsel-sony-psp/debug/* debug/
+fi
