06669748639e0d5e7110f584f46e18d4d1f99471
Glenn Hope <glenn.alexander.hope@gmail.com>
2020-07-19 19:24:54 -0700

    Add sin function and tests for it

--- ci/tests/src/math_test.rs
+++ ci/tests/src/math_test.rs
@@ -1,14 +1,34 @@
 use psp::math;
+use psp::sys::GU_PI;
 use psp::test_runner::TestRunner;
+
+const EPSILON: f32 = 0.00001;
 
 pub fn test_main(test_runner: &mut TestRunner) {
     test_runner.check_list(&[
         ("cos_2.5", test_cos(2.5), -0.8011436),
         ("cos_0", test_cos(0.0), 1.0),
-        ("cos_pi", test_cos(psp::sys::GU_PI), -1.0),
+        ("cos_pi", test_cos(GU_PI), -1.0),
+        ("intrinsics_cos_pi", test_cos_intrinsic(GU_PI), -1.0),
+
+        ("sin_2.5", test_sin(2.5), 0.5984721),
     ]);
+    let almost_zero = test_sin_intrinsic(GU_PI);
+    test_runner.check_true("intrinsics_sin_0", almost_zero < EPSILON && almost_zero > -EPSILON);
 }
 
 fn test_cos(num: f32) -> f32 {
     unsafe { math::cosf32(num) }
+}
+
+fn test_cos_intrinsic(num: f32) -> f32 {
+    unsafe { core::intrinsics::cosf32(GU_PI) }
+}
+
+fn test_sin(num: f32) -> f32 {
+    unsafe { math::sinf32(num) }
+}
+
+fn test_sin_intrinsic(num: f32) -> f32 {
+    unsafe { core::intrinsics::sinf32(GU_PI) }
 }
--- psp/src/math/trig.rs
+++ psp/src/math/trig.rs
@@ -1,23 +1,29 @@
 use core::mem::MaybeUninit;
 
+macro_rules! trig_func {
+    ( $($name:ident),* ) => {
+        $( item! {
+            #[allow(non_snake_case)]
+            #[no_mangle]
+            pub unsafe extern "C" fn [< $name f32 >](rad: f32) -> f32 {
+                let mut out = MaybeUninit::uninit();
 
-// TODO: cosf vs cosf32? which makes intrinsics::cosf32 work?
-#[allow(non_snake_case)]
-#[no_mangle]
-pub unsafe extern "C" fn cosf32(rad: f32) -> f32 {
-    let mut out = MaybeUninit::uninit();
+                $crate::vfpu_asm!(
+                    .mips "mfc1 $$t0, $1";
+                    mtv t0, S000;
+                    vcst_s S001, VFPU_2_PI;
+                    vmul_s S000, S000, S001;
+                    [< v $name _s>] S000, S000;
+                    mfv v0, S000;
+                    .mips "mtc1 $$v0, $1";
 
-    vfpu_asm!(
-        .mips "mfc1 $$t0, $1";
-        mtv t0, S000;
-        vcst_s S001, VFPU_2_PI;
-        vmul_s S000, S000, S001;
-        vcos_s S000, S000;
-        mfv v0, S000;
-        .mips "mtc1 $$v0, $1";
+                    : "={v0}"(out.as_mut_ptr()) : "{t0}"(rad) : "memory" : "volatile"
+                );
 
-        : "={v0}"(out.as_mut_ptr()) : "{t0}"(rad) : "memory" : "volatile"
-    );
-
-    out.assume_init()
+                out.assume_init()
+            }
+        })*
+    };
 }
+
+trig_func! {cos, sin}
