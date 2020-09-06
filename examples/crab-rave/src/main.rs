d342dce670e83467ecbe0edb05218cc28ef71763
Paul Sajna <sajattack@gmail.com>
2020-09-05 19:24:07 -0700

    solid model instead of seethrough triangles

--- examples/crab-rave/src/main.rs
+++ examples/crab-rave/src/main.rs
@@ -145,8 +145,8 @@ fn psp_main() {
 
             sys::sceGumDrawArray(
                 GuPrimitive::Triangles,
-                VertexType::VERTEX_32BITF | VertexType::TRANSFORM_3D,
-                crab::VERTICES.0.len() as i32,
+                VertexType::VERTEX_32BITF | VertexType::TRANSFORM_3D | VertexType::INDEX_16BIT,
+                crab::INDICES.0.len() as i32,
                 //core::ptr::null(),
                 &crab::INDICES as *const _ as *const _, 
                 &crab::VERTICES as *const _ as *const _,
