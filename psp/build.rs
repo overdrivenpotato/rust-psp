6faa1f93d98c9ea1b5a6762cb28407a8e07612cd
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2022-06-07 15:30:43 -0400

    Use LTO build of libunwind if using -Clinker-plugin-lto
    This greatly reduces binary sizes, at the expense of compilation speed.
    Normal builds still use a regular non-LTO build of libunwind.

--- psp/build.rs
+++ psp/build.rs
@@ -1,18 +1,31 @@
-use std::{env, path::Path};
+use std::{env, path::Path, os::unix::prelude::OsStrExt};
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=libunwind.a");
+    println!("cargo:rerun-if-env-changed=RUSTFLAGS");
 
     if env::var("CARGO_FEATURE_STUB_ONLY").is_ok() {
         return;
     }
 
+    // Figure out whether to use the LTO libunwind, or the regular one.
+    let libunwind = if env::var_os("CARGO_ENCODED_RUSTFLAGS")
+        .expect("could not get `CARGO_ENCODED_RUSTFLAGS` variable")
+        .as_bytes()
+        .split(|b| *b == 0x1f)
+        .any(|flags| flags.starts_with(b"-Clinker-plugin-lto"))
+    {
+        "./libunwind_lto.a"
+    } else {
+        "./libunwind.a"
+    };
+
     // TODO: Do we even need to copy the library over? Maybe we can just link
     // directly from the current directory.
     let out_dir = env::var("OUT_DIR").unwrap();
     let out_file = Path::new(&out_dir).join("libunwind.a");
-    std::fs::copy("./libunwind.a", out_file).unwrap();
+    std::fs::copy(libunwind, out_file).unwrap();
 
     println!("cargo:rustc-link-lib=static=unwind");
     println!("cargo:rustc-link-search=native={}", out_dir);
