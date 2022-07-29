95c68a9dde4e9aa15736154e83d2bad755b98406
Paul Sajna <hello@paulsajna.com>
2022-07-29 11:10:45 -0700

    semicolons

--- psp/build.rs
+++ psp/build.rs
@@ -1,8 +1,8 @@
 use std::{env, path::Path};
 #[cfg(target_family = "unix")]
-use std::os::unix::prelude::OsStrExt
+use std::os::unix::prelude::OsStrExt;
 #[cfg(target_family = "windows")]
-use std::os::windows::prelude::OsStrExt
+use std::os::windows::prelude::OsStrExt;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
