9f488733bd15ae6aa5f084284a67d534e6aef54f
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2022-07-31 21:22:22 -0400

    Conditionally use CARGO_HOME cache on non-mac systems

--- ci/concourse/build-rust.sh
+++ ci/concourse/build-rust.sh
@@ -5,15 +5,21 @@ set -euo pipefail
 # If NO_CACHE is *not* set, then setup the cache directories
 if [ -z "${NO_CACHE:-}" ]; then
     # Cache only for normal builds
-    export CARGO_HOME="$(pwd)"/.cargo
+
+    # macOS Concourse runners make a new volume every time a task is run.
+    # These volumes are named randomly every time, which changes where crate
+    # source files are stored. This leads cargo to believe the files were
+    # changed, so it triggers a full rebuild. Instead, we can use the default
+    # directory for macOS runners.
+    #
+    # On Linux, Concourse re-uses container paths so this issue is not relevant.
+    # This is probably either a Concourse or Cargo bug.
+    if ! [ $(uname) = Darwin ]; then
+        export CARGO_HOME="$(pwd)"/.cargo
+    fi
+
     export RUSTUP_HOME="$(pwd)"/.rustup
 fi
-
-pwd
-echo CARGO_HOME: $CARGO_HOME
-echo RUSTUP_HOME: $RUSTUP_HOME
-
-sleep 1000000
 
 rustup set profile minimal
 rustup update --no-self-update $RUSTUP_TOOLCHAIN
