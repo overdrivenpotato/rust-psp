f997edb5055ebb0b98c76647d9207345a04b78b3
Paul Sajna <hello@paulsajna.com>
2022-07-31 18:50:36 -0700

    Add ci for macOS (#140)
    Co-authored-by: Marko Mijalkovic <marko.mijalkovic97@gmail.com>

--- ci/concourse/build-rust.sh
+++ ci/concourse/build-rust.sh
@@ -5,9 +5,20 @@ set -euo pipefail
 # If NO_CACHE is *not* set, then setup the cache directories
 if [ -z "${NO_CACHE:-}" ]; then
     # Cache only for normal builds
-    export CARGO_HOME="$(pwd)"/.cargo
-    export XARGO_HOME="$(pwd)"/.xargo
-    export RUSTUP_HOME="$(pwd)"/.rustup
+
+    # macOS Concourse runners make a new volume every time a task is run.
+    # These volumes are named randomly every time, which changes where crate
+    # source files are stored. This leads cargo to believe the files were
+    # changed, so it triggers a full rebuild. Instead, we can use the default
+    # directory for macOS runners. The rust-src component is also affected as
+    # well, so we must exclude both CARGO_HOME and RUSTUP_HOME.
+    #
+    # On Linux, Concourse re-uses container paths so this issue is not relevant.
+    # This is probably either a Concourse or Cargo bug.
+    if ! [ $(uname) = Darwin ]; then
+        export CARGO_HOME="$(pwd)"/.cargo
+        export RUSTUP_HOME="$(pwd)"/.rustup
+    fi
 fi
 
 rustup set profile minimal
@@ -34,11 +45,15 @@ pushd repo/cargo-psp/
 cargo build
 popd
 
-PATH="$(realpath repo)/target/debug:$PATH"
+PATH="$(pwd)/repo/target/debug:$PATH"
 
 # build the test project
 pushd repo/ci/tests
 cargo psp
 popd
+
+# Make the output directory, in case it is not specified as an output for this
+# concourse task.
+mkdir -p rust-build-dir
 
 cp -r repo/ci/tests/target/mipsel-sony-psp/debug/* rust-build-dir
