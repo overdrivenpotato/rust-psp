8fd9050a935d260c31192aaa61501e49a9f529a9
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-06-19 19:53:08 +0100

    fix: day version lags behind 1 day

--- cargo-psp/src/main.rs
+++ cargo-psp/src/main.rs
@@ -142,7 +142,7 @@ impl core::ops::Add<CommitDate> for CommitDate {
 const MINIMUM_COMMIT_DATE: CommitDate = CommitDate {
     year: 2024,
     month: 6,
-    day: 18,
+    day: 17,
 };
 const MINIMUM_RUSTC_VERSION: Version = Version {
     major: 1,
